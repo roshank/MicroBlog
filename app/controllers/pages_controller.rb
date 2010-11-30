ec99c84f42fac94ed7b4109cda0a51ea54b1efb3
Roshan Khan <roshan.khan@gmail.com>
2010-11-29 23:43:14 -0800

    incorporating first round of Amazon search into about page. Yes, kind of wierd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
+require 'amazon/ecs'
 class PagesController < ApplicationController
+  Amazon::Ecs.options = {:aWS_access_key_id => "AKIAJUFAFIGRO6EFG6EQ", 
+                         :aWS_secret_key => "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"}
   def home
     @title = "Home"
     if signed_in?
@@ -11,7 +14,14 @@ class PagesController < ApplicationController
   end
   
   def about
-    @title = "About"
+    queryText = params[:q]
+    if (queryText == nil)
+      @title = 'About'
+    else
+      @title = params[:q]
+      res = Amazon::Ecs.item_search(@title, {:response_group => "Medium", :sort => "salesrank", :item_page => 1})
+      @items = res.items
+    end
   end
   
   def help
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,5 @@
 module PagesHelper
+  def item_image(url, alt)
+    image_tag(url, :alt => alt, :class => "round")
+  end
 end
